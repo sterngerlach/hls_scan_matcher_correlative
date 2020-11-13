751a7973aff74db8d08bf71f8a461ae30922c078
Stern Gerlach <std.experimental.optional@gmail.com>
2020-11-13 12:06:41 +0900

    Add tcl source files for synthesis

---
+++ common.tcl
@@ -0,0 +1,20 @@
+
+# common.tcl
+
+open_project -reset scan_matcher_correlative
+set_top ScanMatchCorrelative
+
+add_files scan_matcher_correlative/main.cpp
+add_files scan_matcher_correlative/main.h
+add_files scan_matcher_correlative/reduce.cpp
+add_files scan_matcher_correlative/reduce.h
+
+add_files -tb scan_matcher_correlative/pgm.cpp \
+    -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb scan_matcher_correlative/pgm.h \
+    -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+
+open_solution -reset "solution"
+set_part {xc7z020-clg400-1}
+create_clock -period 10 -name default
+
---
+++ csynth.tcl
@@ -0,0 +1,6 @@
+
+# csynth.tcl
+
+source "scan_matcher_correlative/common.tcl"
+csynth_design
+
